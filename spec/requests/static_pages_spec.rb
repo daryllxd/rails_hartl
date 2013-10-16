886ddc784493cab0a55060aef922e871127f91de
Daryll Santos <daryll.santos@gmail.com>
2013-10-17 03:44:58 +0800

    [Layout] Added helper for title page.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,7 +10,7 @@ describe "StaticPages" do
     
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("#{base_title} | Home")
+      expect(page).not_to have_title("| Home")
     end
   end
   
